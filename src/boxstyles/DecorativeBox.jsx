1e46b1f94dc48a2465aca5f7cf0fe2494b15ae45
Ignaciovega1 <Ivega@utem.cl>
2023-10-17 12:04:56 -0300

    restart commit

---
+++ src/boxstyles/DecorativeBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DecorativeBox = ({ children }) => {
+  return (
+    <div className="decorative-box">
+      {children}
+    </div>
+  );
+}
+
+export default DecorativeBox;
---
+++ src/boxstyles/Totalvuelo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Totalvuelo = () => {
+  return (
+    <div className="Totalvuelo-box">
+      <div className="texto-izquierda">
+        <div className="texto-con-trazo">
+      <h3>Total del Vuelo</h3>
+      <hr></hr>
+       </div>
+      </div>
+    </div>
+  );
+}
+
+export default Totalvuelo;
