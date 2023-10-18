60c4b0b4cb769f6d973a719015364b354956800a
Ignaciovega1 <Ivega@utem.cl>
2023-10-18 18:00:17 -0300

    18-10 commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,4 +4,10 @@ window .addEventListener('load', function() {
 
 preloaderWrapper.classList.add('fade-out-animation');
 
+setTimeout(() => {
+
+preloaderWrapper.style.opacity = '0';
+
+}, 1000);
+
 });
