aba692699bf8deff439699fe661f074ecd0802fb
GiovannyxDDD <gvargasp@utem.cl>
2023-10-27 17:15:32 -0300

    Cambio

--- src/components/Modalcomponent.jsx
+++ src/components/Modalcomponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import jsPDF from 'jspdf';
+import './ModalComponent.css';
 
 const ModalComponent = () => {
 
@@ -27,16 +28,23 @@ const ModalComponent = () => {
   return (
     <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div className="modal-dialog modal-lg">
-        <div className="modal-content" style={{ maxHeight: "80vh" }}> {/* Ajusta el valor maxHeight según tus necesidades */}
+        <div className="modal-content" style={{ maxHeight: "80vh" }}>
           <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">Título del modal</h5>
+            <h5 className="modal-title" id="exampleModalLabel">RESUMEN DE DATOS</h5>
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div className="modal-body">
-            Contenido...
+            <div className="content-wrapper">
+              <div className="content-image">
+                <img src="/logo.png" alt="Logo" style={{ width: '100px', height: '100px' }} />
+              </div>
+              <div className="vertical-line"></div>
+              <div className="content-text">
+                Contenido...
+              </div>
+            </div>
           </div>
           <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
             <button type="button" className="btn btn-primary" onClick={crearPDF}>Descargar documento PDF</button>
           </div>
         </div>
