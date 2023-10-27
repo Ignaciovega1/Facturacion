7fb12820469e5b8afb3914b7e3e2a212b696b2f0
Ignaciovega1 <Ivega@utem.cl>
2023-10-26 23:17:23 -0300

    26-10 commit

--- src/components/Compra.jsx
+++ src/components/Compra.jsx
@@ -11,13 +11,18 @@ function Compra({ objeto }) { // Cambiamos props a objeto
                 <p className="fw-bold text-light">Detalle de la compra</p>
             </div>
             <hr />
-            <p className="text-light">Ciudad de Inicio: {vueloInfo.ciudad_inicio}</p>
-            <p className="text-light">Ciudad de Fin: {vueloInfo.ciudad_fin}</p>
-            <p className="text-light">Tipo de Vuelo: {vueloInfo.tipo_vuelo}</p>
-            <p className="text-light">Fecha de Ida: {vueloInfo.fecha_ida}</p>
-            <p className="text-light">Fecha de Vuelta: {vueloInfo.fecha_vuelta}</p>
-            <p className="text-light">Hora de Ida: {vueloInfo.hora_ida}</p>
-            <p className="text-light">Hora de Fin: {vueloInfo.hora_fin}</p>
+            <p className="text-secondary fs-6 mb-0">Ciudad de Inicio</p> 
+            <p className="text-light fs-3">{vueloInfo.ciudad_inicio}</p>
+            <p className="text-dark fs-6 mb-0">Ciudad de Fin</p> 
+            <p className="text-light fs-3">{vueloInfo.ciudad_fin}</p>
+            <p className="text-dark fs-6 mb-0">Tipo de Vuelo</p> 
+            <p className="text-light fs-3">{vueloInfo.tipo_vuelo}</p>
+            <p className="text-light fs-6 mb-0">Ida</p> 
+            <p className="text-light fs-6">{vueloInfo.fecha_ida}</p>
+            <p className="text-light fs-6 mb-0">Vuelta</p> 
+            <p className="text-light fs-6">{vueloInfo.fecha_vuelta}</p>
+            <p className="text-light fs-6">Hora de Ida: {vueloInfo.hora_ida}</p>
+            <p className="text-light fs-6">Hora de Fin: {vueloInfo.hora_fin}</p>
             <p className="text-light">Escalas: {vueloInfo.escalas}</p>
         </div>
     );
